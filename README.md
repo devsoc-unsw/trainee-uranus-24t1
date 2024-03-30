96ba7c2b0846ce0e692b06caa6ba3228ae85462b
Technotech <78465863+TechnotechGit@users.noreply.github.com>
2024-03-30 13:08:03 +1100

    Initialise project structure

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ![Important](https://media.tenor.com/q0Ejci9EQhcAAAAi/rick-astley-rick-roll.gif)
+
+### Unswipe
---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import {
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	Link,
+} from "react-router-dom";
+import "./App.css";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: (
+			<div>
+				<h1>Hello World</h1>
+				<Link to="about">About Us</Link>
+			</div>
+		),
+	},
+	{
+		path: "about",
+		element: <div>About</div>,
+	},
+]);
+
+function App() {
+	return (
+		<>
+			<RouterProvider router={router} />
+		</>
+	);
+}
+
+export default App;
