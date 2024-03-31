da9aa067c9a69bb699bae71ba5dc0291eebcf95b
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-04-01 10:22:43 +1100

    Init backend

---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`UNSWipe-backend listening at http://localhost:${port}`);
+});
