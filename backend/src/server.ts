1cf77c532e5b4bf7cbad2123a667f1957d3339be
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-04-01 22:46:36 +1100

    Add configure MongoDB Atlas

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,39 @@
 import express from 'express';
+
+
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+/*
+  Interface sketch
+
+  - user registration
+    - Course code list
+      - I am going to let the users be responsible 
+        for checking if each course is available 
+        in any given term.
+    - Programming languages list
+    - Other information
+      - Languages
+      - Gender
+      - Age
+      - WAM bracket
+      - Academic Social Ratio
+    - Hobbies list
+    - Preferences (see other information)
+  - user login
+
+  - Preview self profile
+  - Modify self profile (see registration)
+
+  - Matching
+    - Receive the next `n` recommended profiles
+    - Send back to the server when matched
+
+  - Messaging Socket.IO magic
+*/
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
