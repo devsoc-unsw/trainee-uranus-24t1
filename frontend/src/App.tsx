82376369d5c98cf54b5a1eb6803341f823f324f8
Technotech <technotech2121@gmail.com>
2024-04-04 16:48:43 +1100

    Add colour palette from Figma

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,27 +3,27 @@ import "./App.css";
 import AppProvider from "./contexts/AppContext";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <div>
-        <h1>Hello World</h1>
-        <Link to="about">About Us</Link>
-      </div>
-    ),
-  },
-  {
-    path: "about",
-    element: <div>About</div>,
-  },
+    {
+        path: "/",
+        element: (
+            <div>
+                <h1 className="text-3xl text-primary-500">Hello World</h1>
+                <Link to="about">About Us</Link>
+            </div>
+        ),
+    },
+    {
+        path: "about",
+        element: <div>About</div>,
+    },
 ]);
 
 function App() {
-  return (
-    <AppProvider>
-      <RouterProvider router={router} />
-    </AppProvider>
-  );
+    return (
+        <AppProvider>
+            <RouterProvider router={router} />
+        </AppProvider>
+    );
 }
 
 export default App;
