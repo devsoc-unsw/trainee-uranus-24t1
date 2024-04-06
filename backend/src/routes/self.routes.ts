91108b214843d0f2edc650913428065e8751b94f
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-04-07 01:39:14 +1100

    Add self-information routes

---
+++ backend/src/routes/self.routes.ts
@@ -0,0 +1,29 @@
+import express, { Request, Response, query } from "express";
+import { collections } from "../services/database.service";
+import User from "../models/user";
+import { requireToken } from "../middleware/token.middleware";
+
+export const selfRouter = express.Router();
+selfRouter.use(express.json());
+
+selfRouter.get('/', requireToken, async (req: Request, res: Response) => {
+  res.status(200).json(req.user);
+});
+
+selfRouter.put('/', requireToken, async (req: Request, res: Response) => {
+  try {
+    const updatedUser: User = req.body as User;
+    // Do not update _id
+    const {_id, ...withoutId} = updatedUser;
+    const result = await collections.users?.updateOne({ _id: req.user._id! }, { $set: withoutId });
+    
+    if (result) {
+      res.status(200).send('Successfully updated user');
+    } else {
+      res.status(304).send('Could not update user');
+    }
+  } catch (error: any) {
+    console.error(error);
+    res.status(400).send(error.message);
+  }
+});
