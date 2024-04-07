0e1769b43cec47d374b0998d2e7139429dd60bd5
Jacqueline Ta <jxcquelineta@gmail.com>
2024-04-08 04:53:09 +1000

    Removed backtick and braces around isClicked ternary and added separate variable for the disabled and isClicked condition

--- frontend/src/components/CourseButton.tsx
+++ frontend/src/components/CourseButton.tsx
@@ -8,6 +8,7 @@ interface CourseButtonProps {
     course: string;
     disabled: boolean;
 }
+
 const CourseButton: React.FC<CourseButtonProps> = ({ course, disabled }) => {
     const [isClicked, setIsClicked] = useState(false);
 
@@ -17,15 +18,13 @@ const CourseButton: React.FC<CourseButtonProps> = ({ course, disabled }) => {
         }   
     };
 
+    const dynamicStyle = disabled ? 'text-tertiary-500 bg-tertiary-bg-500 border-tertiary-bg-500' : 
+    isClicked ? 'bg-secondary-bg-500 text-white' : 'text-secondary-bg-500 bg-white hover:bg-secondary-bg-500 hover:text-white';
+
     return (
-        <button
-            className={`rounded-full  px-6 py-2  border-2 border-secondary-bg-500 
-            ${disabled ? 'text-tertiary-500 bg-tertiary-bg-500 border-tertiary-bg-500' : 
-            `${isClicked ? 'bg-secondary-bg-500 text-white ' : 'text-secondary-bg-500 bg-white'}} 
-            hover:text-white hover:bg-secondary-bg-500`}`} 
-            onClick={!disabled ? handleClick : undefined}
-            >
-        {course}
+        <button className={`rounded-full  px-6 py-2  border-2 border-secondary-bg-500 ${dynamicStyle}`} 
+            onClick={!disabled ? handleClick : undefined}>
+            {course}
         </button>
     )
 } 
