db5741f1bd4fb200f15bad76c23bc2385728231c
Chinosu <46995931+Chinosu@users.noreply.github.com>
2024-04-11 18:45:50 +1000

    Add whitespace

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -4,6 +4,7 @@ export default class User {
   constructor(
     public firstName: string,
     public lastName: string,
+    
     public email: string,
     public password: string,
 
@@ -27,4 +28,24 @@ export default class User {
 
     public _id?: ObjectId,
   ) {}
+}
+
+export function unpackUser(user: User) {
+  const { firstName, lastName, email, password, courses, futureCourses, hobbies, languages, programmingLanguages, gender, age, wam, academicSocialRatio } = user;
+
+  console.log({
+    firstName,
+    lastName,
+    email,
+    password,
+    courses,
+    futureCourses,
+    hobbies,
+    languages,
+    programmingLanguages,
+    gender,
+    age,
+    wam,
+    academicSocialRatio
+  });
 }
