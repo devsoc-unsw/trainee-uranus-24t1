deb2a6ec9103f94b5c3fcf1f0d3e465fef45b61c
Technotech <78465863+TechnotechGit@users.noreply.github.com>
2024-04-11 17:31:35 +1000

    Fix styling
    Quick style edit

--- frontend/src/components/CourseButton.tsx
+++ frontend/src/components/CourseButton.tsx
@@ -19,10 +19,10 @@ const CourseButton: React.FC<CourseButtonProps> = ({ course, disabled }) => {
     };
 
     const dynamicStyle = disabled ? 'text-tertiary-500 bg-tertiary-bg-500 border-tertiary-bg-500' : 
-    isClicked ? 'bg-secondary-bg-500 text-white' : 'text-secondary-bg-500 bg-white hover:bg-secondary-bg-500 hover:text-white';
+    isClicked ? 'bg-secondary-bg-500 border-secondary-bg-600 text-white' : 'text-secondary-bg-500 bg-white hover:bg-secondary-bg-500 hover:text-white';
 
     return (
-        <button className={`rounded-full  px-6 py-2  border-2 border-secondary-bg-500 ${dynamicStyle}`} 
+        <button className={`rounded-full  transition-all duration-250  px-6 py-2  border-2 border-secondary-bg-500 outline-none ${dynamicStyle}`} 
             onClick={!disabled ? handleClick : undefined}>
             {course}
         </button>
