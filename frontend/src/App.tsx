8cbe808df4f906abe2bdf9283d04c8e73d003f27
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2024-05-13 14:45:13 +1000

    Fixed indentation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,50 +8,50 @@ import BackButton from "./components/BackButton";
 import AdaptableButton from "./components/AdaptableButton";
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: (
-            <div>
-                <h1 className="text-3xl text-primary-500">Hello World</h1>
-                <Link to="about">About Us</Link>
-                <br/>
-                <AdaptableButton label='COMP1531' disabled={false}/>
-                <AdaptableButton label='COMP1511' disabled={true}/>
-                <br/>
+  {
+    path: "/",
+    element: (
+      <div>
+        <h1 className="text-3xl text-primary-500">Hello World</h1>
+        <Link to="about">About Us</Link>
+        <br/>
+        <AdaptableButton label='COMP1531' disabled={false}/>
+        <AdaptableButton label='COMP1511' disabled={true}/>
+        <br/>
 
-                <BackButton/>
-                <br/>
-                <AdaptableButton label="hi" disabled={false} showDash/>
-                <AdaptableButton label="goodsoup"/>
+        <BackButton/>
+        <br/>
+        <AdaptableButton label="hi" disabled={false} showDash/>
+        <AdaptableButton label="goodsoup"/>
 
-                <br/>
+        <br/>
 
-                <Link to="register">Register</Link>
-            </div>
-        ),
-    },
-    {
-        path: "about",
-        element: <div>About</div>,
-    },
-    {
-        path: "register",
-        element: <RegisterPage />,
-    },
-    {
-        path: "login",
-        element: <LoginPage />,
-    },
+        <Link to="register">Register</Link>
+      </div>
+    ),
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
 ]);
 
 function App() {
-    return (
-        <AppProvider>
-            <div className="p-10 text-black">
-                <RouterProvider router={router} />
-            </div>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <div className="p-10 text-black">
+        <RouterProvider router={router} />
+      </div>
+    </AppProvider>
+  );
 }
 
 export default App;
