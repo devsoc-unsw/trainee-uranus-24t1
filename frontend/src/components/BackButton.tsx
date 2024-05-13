c74f1ce680525aa0374781bd3827200f08feaaa6
Jacqueline Ta <z5361055@ad.unsw.edu.au>
2024-05-13 14:46:53 +1000

    Changed react import name to pascal case

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -1,9 +1,10 @@
-import backIcon from '../../images/backIcon.png'
+import BackIcon from '../../images/backIcon.png'
+
 const BackButton = () => {
   return (
     <button style={{ width: '45px', height: '45px', display: 'inline-flex', alignItems: 'center', justifyContent: 'center' }}
     className={"rounded-full bg-secondary-bg-500"}>
-    <img src={backIcon} alt="Back" style={{ width: '60%', height: '35%', filter: 'invert(100%)'}} className="fill-white"/> 
+    <img src={BackIcon} alt="Back" style={{ width: '60%', height: '35%', filter: 'invert(100%)'}} className="fill-white"/> 
     </button>
   )
 }
